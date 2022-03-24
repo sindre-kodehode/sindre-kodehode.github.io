ceb9ed1771fc107bc0e283e652e8fb31e051144f
sindre-kodehode <sindre.kodehode@gmail.com>
2022-03-24 16:43:58 +0100

    chrome extension

--- 05-chrome-extension/index.js
+++ 05-chrome-extension/index.js
@@ -1,23 +1,62 @@
+"use strict"
+
+const deleteBtn = document.getElementById('delete-btn');
+const inputBtn  = document.getElementById('input-btn');
+const inputEl   = document.getElementById('input-el');
+const tabBtn    = document.getElementById('tab-btn');
+const ulEl      = document.getElementById('ul-el');
+
+const leadsFromStorage = localStorage.getItem('myLeads');
+
 let myLeads = [];
 
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+if (leadsFromStorage) {
+  myLeads = JSON.parse(leadsFromStorage);
+  render(myLeads);
+}
 
-inputBtn.addEventListener("click", renderLead);
+inputBtn.addEventListener('click', addLead);
+deleteBtn.addEventListener('click', deleteLeads);
+tabBtn.addEventListener('click', saveTab)
 
-function renderLead() {
+function addLead() {
   myLeads.push(inputEl.value);
+  inputEl.value = '';
 
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li>
-                    <a href="https://${myLeads[i]}" target="_blank">
-                      ${myLeads[i]}
-                    </a>
-                  </li>`;
+  saveLeads();
+  render(myLeads);
+}
+
+function saveLeads() {
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+  console.log('hello');
+}
+
+function saveTab() {
+  console.log('hello');
+  browser.tabs.get().url;
+
+  myLeads.push(browser.tabs);
+  saveLeads();
+  render(myLeads);
+}
+
+function deleteLeads() {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+}
+
+function render(list) {
+  let listItems = '';
+  for (const li of list) {
+    listItems += `
+    <li>
+      <a href="https://${li}" target="_blank">
+        ${li}
+      </a>
+    </li>`;
   }
 
-  inputEl.value = "";
   ulEl.innerHTML = listItems;
 }
