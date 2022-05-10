4215faaa7b12613d98444ae8f6c6bafd63752f9a
sindre-kodehode <sindre.kodehode@gmail.com>
2022-05-10 22:28:34 +0200

    add OOP solution

--- task-12-js-drum-kit/main.js
+++ task-12-js-drum-kit/main.js
@@ -17,11 +17,39 @@ drums.forEach( drum => {
   const element = document.createElement( "div" );
 
   element.textContent = drum.type;
-  document.body.appendChild( element );
-
   element.addEventListener( "mousedown", () => playDrum( drum.type ) );
 
+  document.body.appendChild( element );
   document.addEventListener( "keydown", e => {
     if ( e.key === drum.key ) playDrum( drum.type );
   });
 });
+
+/*******************************************************************************
+* class Drum {                                                                 * 
+*   constructor( type, key ) {                                                 * 
+*     this.type = type;                                                        * 
+*     this.key  = key;                                                         * 
+*                                                                              * 
+*     this.element = document.createElement( "div" );                          * 
+*     this.element.textContent = this.type;                                    * 
+*     this.element.addEventListener( "mousedown", () => this.play() );         * 
+*                                                                              * 
+*     document.body.appendChild( this.element );                               * 
+*     document.addEventListener( "keydown", e => {                             * 
+*       if ( e.key === this.key ) this.play();                                 * 
+*     });                                                                      * 
+*   }                                                                          * 
+*                                                                              * 
+*   play() { new Audio( `./sound/${this.type}.wav` ).play(); }                 * 
+* }                                                                            * 
+*                                                                              * 
+* new Drum( "clap"    , "s" );                                                 * 
+* new Drum( "hihat"   , "d" );                                                 * 
+* new Drum( "kick"    , "f" );                                                 * 
+* new Drum( "openhat" , "g" );                                                 * 
+* new Drum( "ride"    , "h" );                                                 * 
+* new Drum( "snare"   , "j" );                                                 * 
+* new Drum( "tink"    , "k" );                                                 * 
+* new Drum( "tom"     , "l" );                                                 * 
+*******************************************************************************/
