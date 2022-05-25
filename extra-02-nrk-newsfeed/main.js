2bfa112988a02b58ae7210a8acd2d631d8d7c308
sindre-kodehode <sindre.kodehode@gmail.com>
2022-05-25 22:48:38 +0200

    change to classes for nrk app

--- extra-02-nrk-newsfeed/main.js
+++ extra-02-nrk-newsfeed/main.js
@@ -2,122 +2,119 @@
 
 
 /*******************************************************************************
+* Article class                                                                *
+*******************************************************************************/
+class Article {
+  constructor({ id, lead, time, title, compilations, attachment }) {
+    this.id = id;
+
+    this.listEl = document.createElement( "p" );
+    this.listEl.textContent = `${time} ${title}`;
+
+    this.articleEl = document.createElement( "article" );
+    const titleEl  = document.createElement( "h2"      );
+    const infoEl   = document.createElement( "p"       );
+    const leadEl   = document.createElement( "p"       );
+    const imageEl  = document.createElement( "img"     );
+
+    this.articleEl.textContent = "";
+    titleEl.textContent = title;
+    leadEl.innerHTML    = lead;
+    leadEl.innerHTML    = leadEl.textContent;
+    infoEl.textContent  = `${time}`;
+
+    if ( compilations )
+      infoEl.textContent += ` ${compilations[0].title}`;
+
+    this.articleEl.append( titleEl );
+    this.articleEl.append( infoEl );
+
+    if ( attachment ) {
+      imageEl.src = attachment.image;
+      this.articleEl.append( imageEl );
+    }
+
+    this.articleEl.append( leadEl );
+  }
+
+  select()   { this.listEl.classList.add(    "selected" ); }
+  unselect() { this.listEl.classList.remove( "selected" ); }
+}
+
+
+/*******************************************************************************
+* ArticleList class                                                            *
+*******************************************************************************/
+class ArticleList {
+  constructor() {
+    this.articles = [];
+    this.selected = 0;
+
+    this.articlesEl = document.createElement( "div" );
+    this.articlesEl.classList.add( "articles" );
+    document.body.append( this.articlesEl );
+
+    this.viewEl = document.createElement( "article" );
+    this.viewEl.classList.add( "view" );
+    document.body.append( this.viewEl );
+  }
+
+  get length() { return this.articles.length }
+
+  push({ messages }) {
+    messages.forEach( article => {
+      if ( !this.articles.find( a => a.id === article.id ) )
+        this.articles.push( new Article( article ));
+    });
+
+    this.articlesEl.textContent = "";
+    this.articles.forEach( article => this.articlesEl.append( article.listEl ) );
+    this.selectFirst();
+  }
+
+  selectFirst() { this.select( 0 ); };
+  selectLast()  { this.select( this.length - 1 ); };
+  selectNext()  { this.select( this.selected + 1 ); };
+  selectPrev()  { this.select( this.selected - 1 ); };
+
+  select( next ) {
+    this.articles[ this.selected ].unselect();
+
+    this.selected = next;
+
+    if ( this.selected < 0 ) this.selected = 0;
+    if ( this.selected > this.length - 1 ) this.selected = this.length - 1;
+
+    this.articles[ this.selected ].select();
+
+    this.viewEl.textContent = "";
+    this.viewEl.append( this.articles[ this.selected ].articleEl );
+  }
+}
+
+
+/*******************************************************************************
 * server sent event source                                                     *
 *******************************************************************************/
 const url = "https://nrkno-ssenotifier.nrk.no/sse/newsfeed";
 const eventSource = new EventSource( url );
-
-
-/*******************************************************************************
-* DOM elements                                                                 *
-*******************************************************************************/
-const articlesElement = document.querySelector( ".articles" );
-const viewElement     = document.querySelector( ".view"     );
-
-
-/*******************************************************************************
-* global variables                                                             *
-*******************************************************************************/
-let articles;
-let data;
-let selected = 0;
-let pressedG = false;
+const articleList = new ArticleList();
 
 
 /*******************************************************************************
 * message recived event                                                        *
 *******************************************************************************/
-eventSource.onmessage = event => {
-  data = JSON.parse( event.data );
-
-  articlesElement.textContent = "";
-
-  for ( let message of data.messages ) {
-    const infoElement = document.createElement( "p" );
-
-    infoElement.textContent  = `${message.time} ${message.title}`
-    articlesElement.appendChild( infoElement );
-  }
-  
-  articles = document.querySelectorAll( ".articles p" );
-  articles[selected].classList.toggle( "selected" );
-  viewArticle();
-};
+eventSource.onmessage = event => articleList.push( JSON.parse( event.data ) );
 
 
 /*******************************************************************************
 * keypress events                                                              *
 *******************************************************************************/
-document.addEventListener( "keydown", event => {
-  switch ( event.key) {
-    case "j" : selectArticle( selected + 1 );        break;
-    case "k" : selectArticle( selected - 1 );        break;
-    case "g" : checkG();                             break;
-    case "G" : selectArticle( articles.length - 1 ); break;
+document.addEventListener( "keydown", ({ key }) => {
+  switch ( key ) {
+    case "g" : articleList.selectFirst(); break;
+    case "G" : articleList.selectLast();  break;
+    case "j" : articleList.selectNext();  break;
+    case "k" : articleList.selectPrev();  break;
   }
 });
-
-const checkG = () => {
-  if ( pressedG ) {
-    selectArticle( 0 );
-  } else {
-    pressedG = true;  
-    setTimeout( () => pressedG = false, 500 );
-  }
-};
-
-
-/*******************************************************************************
-* highlight selected article                                                   *
-*******************************************************************************/
-const selectArticle = ( article ) => {
-  articles[selected].classList.toggle( "selected" );
-
-  selected = article;
-
-  if ( selected < 0 ) {
-    selected = 0;
-  }
-
-  if ( selected > articles.length - 1 ) {
-    selected = articles.length - 1;
-  }
-
-  articles[selected].classList.toggle( "selected" );
-  articles[selected].scrollIntoView( true );
-
-  viewArticle();
-};
-
-
-/*******************************************************************************
-* view selected article view at bottom of the page                             *
-*******************************************************************************/
-const viewArticle = () => {
-  const message = data.messages[selected];
-  const title   = document.createElement( "h2" );
-  const info    = document.createElement( "p"  );
-  const lead    = document.createElement( "p"  );
-
-  title.textContent = `${message.title}`;
-  info.textContent  = `${message.time}`;
-
-  if ( message.compilations ) {
-    info.textContent += ` ${message.compilations[0].title}`;
-  }
-
-  lead.innerHTML = `${message.lead}`;
-
-  viewElement.textContent = "";
-  viewElement.appendChild( title );
-  viewElement.appendChild( info );
-
-  if ( message.attachment ) {
-    const image = document.createElement( "img" );
-    image.src   = message.attachment.image;
-
-    viewElement.appendChild( image );
-  }
-
-  viewElement.appendChild( lead );
-};
