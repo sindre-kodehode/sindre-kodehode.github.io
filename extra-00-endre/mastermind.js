2e851d42b9024ef5cff78d6b47e102c0b251c62b
sindre-kodehode <sindre.kodehode@gmail.com>
2022-06-07 09:10:16 +0200

    add mastermind solution

---
+++ extra-00-endre/mastermind.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+"use strict"
+
+const colors = [
+  "Blue"  , "Green" , "Grey"  , "Orange",
+  "Pink"  , "Red"   , "White" , "Yellow",
+];
+
+const guess1   = [ "Red"   , "Blue"  , "White" , "White", ];
+const guess2   = [ "Pink"  , "White" , "Blue"  , "Green" , ];
+const guess3   = [ "Blue"  , "Blue"  , "White" , "White" , ];
+
+const solution = [ "Red"   , "Grey"  , "White" , "Yellow", ];
+
+const checkGuess = ( guess, solution ) => {
+  let red = 0, white = 0;
+
+  guess.forEach( ( e, i ) => {
+    if      ( e === solution[i]    ) red++;
+    else if ( solution.includes(e) ) white++;
+  });
+  
+  return { Red: red, White: white };
+};
+
+console.log( checkGuess( guess1, solution ) );
+console.log( checkGuess( guess2, solution ) );
+console.log( checkGuess( guess3, solution ) );
