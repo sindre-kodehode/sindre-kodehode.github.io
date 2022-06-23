9b72572526f03ecb8e2eb9188540b942a553d03e
sindre-kodehode <sindre.kodehode@gmail.com>
2022-06-23 14:16:35 +0200

    fix dist?

--- extra-05-endre-react-challenge/vite.config.ts
+++ extra-05-endre-react-challenge/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/extra-05-endre-react-challenge/dist/",
   plugins: [react()],
   server: {
     open: true
