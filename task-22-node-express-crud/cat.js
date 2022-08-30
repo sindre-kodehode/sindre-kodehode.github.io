354233ad2a5b00c78c19f6ee4ad2b83073639b6b
sindre-kodehode <sindre.kodehode@gmail.com>
2022-08-30 09:53:39 +0200

    add cat

--- task-22-node-express-crud/cat.js
+++ task-22-node-express-crud/cat.js
@@ -10,37 +10,27 @@ import * as fs   from "node:fs/promises";
 *  constants                                                                   *
 *******************************************************************************/
 const missingText =
-`mkdir.js: missing operand
+`cat.js: missing operand
 Try 'mkdir.js --help' for more information.
 `;
 
 const helpText =
-`Usage: mkdir.js [OPTION]... DIRECTORY...
-Create the DIRECTORY(ies), if they do not already exist.
+`Usage: cat.js [OPTION]... [FILE]...
+Concatenate FILE(s) to standard output.
 
 Options:
-  -p, --parents     no error if existing, make parent directories as needed
-  -v, --verbose     print a message for each created directory
       --help        display this help and exit
 `
-const createdText = name =>
-  `mkdir.js: created directory '${ name }'`
-
-const fileExistText = name =>
-  `mkdir.js: cannot create directory '${ name }': File exists`
+const invalidText = opt =>
+  `cat.js: invalid option -- '${ opt.replace( /^-*/, "" ) }'`
 
 const pathExistText = name =>
-  `mkdir.js: cannot create directory '${ name }': No such file or directory`
-
-const invalidText = opt =>
-  `mv.js: invalid option -- '${ opt.replace( /^-*/, "" ) }'`
+  `cat.js: cannot create directory '${ name }': No such file or directory`
 
 
 /*******************************************************************************
 *  variables                                                                   *
 *******************************************************************************/
-let parents = false;
-let verbose = false;
 
 
 /*******************************************************************************
@@ -60,18 +50,6 @@ const sources = proc.argv.slice( 2 ).filter( arg => {
     proc.exit( 1 );
   }
 
-  // --parents or -p: create parent directoy(ies) if the path does not exist
-  if ( arg.match( /-p|--parents/ ) ) {
-    parents = true;
-    return false;
-  }
-
-  // --verbose or -v: print message when creating a directoy
-  if ( arg.match( /-v|--verbose/ ) ) {
-    verbose = true;
-    return false;
-  }
-
   // exit and display message if invalid option was given
   else if ( arg.match( /^-/ ) ) {
     console.error( invalidText( arg ) );
@@ -89,23 +67,26 @@ const sources = proc.argv.slice( 2 ).filter( arg => {
 // run for each argument
 for ( const source of sources ) {
   try {
-    // create a new URL with the current directory and the name given
-    await fs.mkdir( new URL( source, import.meta.url ), { recursive : parents } );
+    // create a filehandle of the file given as an argument
+    console.log( "opening file handle" );
+    const fd = await fs.open( new URL( source, import.meta.url ) );
 
-    // show message if verbose
-    if ( verbose ) console.log( createdText( source ) ); 
+    console.log( "creating input stream" );
+    const inputStream  = fd.createReadStream();
+
+    
 
   } catch ( err ) {
-    // error if the directory already exists
-    if ( err.code === "EEXIST" ) console.error( fileExistText( source ) );
-
     // error if the path does not exist
     if ( err.code === "ENOENT" ) console.error( pathExistText( source ) );
+
+    // display if unknown error
+    else console.log( err.message );
 
     // exit with error
     proc.exit( 1 );
   }
 }
 
-// exit with no error after successfully creating directories
+// exit with no error after successfully cating
 proc.exit( 0 );
